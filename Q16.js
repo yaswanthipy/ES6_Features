8020015e70a35cc2ece841b01406012b813835dc
yaswanthi p <yaswanthipy@gmail.com>
2025-11-24 19:05:26 +0530

    a,b,c,

---
+++ Q16.js
@@ -0,0 +1,11 @@
+const isEven = (n) => n % 2 === 0;// Using arrow function
+
+let result = marks >= 35 ? "Pass" : "Fail";// Using ternary operator
+
+const greet = (name) => {// Using default parameters
+  let user = name ? name : "Guest";
+  console.log(`Hello, ${user}`);
+};
+
+
+
